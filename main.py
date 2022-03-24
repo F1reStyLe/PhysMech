da11703d45c589c06c563629c859c01c7b63eaac
F1reStyLe <iwgw@bk.ru>
2022-03-24 12:59:03 +0300

    Пока ничего не понятно, но очень интересно

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
 
 
-class Calculations(Widget):
+class Calculations(BoxLayout):
     pass
 
 
---
+++ models.py
@@ -0,0 +1,51 @@
+
+class ToBend:
+    """Расчет изгибающего напряжения(sigma) и относительной деформации при изгибе(Е).
+        Параметры образца вводятся в метрах"""
+    __length = 0  # расстояние между опорами, М
+    __width = 0  # ширина испытуемого образца, М
+    __height = 0  # толщина образца, М
+    __F = 1
+    __S = 1
+
+    def __init__(self, length=0.064, width=0.013, height=0.04):
+        self.__length = length
+        self.__width = width
+        self.__height = height
+
+    # @property
+    # def get_length(self):
+    #     return self.__length
+    #
+    # @property
+    # def get_width(self):
+    #     return self.__width
+    #
+    # @property
+    # def get_height(self):
+    #     return self.__height
+    #
+    # @property
+    # def get_F(self):
+    #     return self.__F
+
+    @property
+    def get_sigma(self):
+        return round((3 * self.__F * self.__length) / (2 * self.__width * self.__height ** 2), 2)
+
+    @property
+    def get_E(self):
+        return (600 * self.__S * self.__height) / self.__length
+
+    @property
+    def get_anoter_E(self):
+        pass
+
+class Break(ToBend):
+    def __init__(self, *args, **kwargs):
+        super(Break, self).__init__(*args, **kwargs)
+
+
+b = ToBend()
+
+print(b.get_E)
