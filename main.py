89e941cad54f16a9dddf5477d2682f08a7b6c3ef
F1reStyLe <iwgw@bk.ru>
2022-03-24 10:41:24 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class Calculations(Widget):
+    pass
+
+
+
+
+class PhysMechApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    PhysMechApp().run()
